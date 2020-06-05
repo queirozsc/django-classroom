7ce51ed74948fdd897e1694083610fd1cd5f5e38
Sergio Queiroz <queirozsc@gmail.com>
2020-06-05 18:47:01 -0300

    Sets local database to postgres

--- topics/admin.py
+++ topics/admin.py
@@ -1,3 +1,13 @@
+# Python imports
+# Django imports
 from django.contrib import admin
+# Libraries imports
+# App imports
+from .models import Topic
 
-# Register your models here.
+
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ['name', 'course', 'updated_at']
+    search_fields = ['name']
+
+admin.site.register(Topic, TopicAdmin)
